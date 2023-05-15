ff49432cccef3d36a175e88372281713ee38f19c
Ahmed Omar Salim Adnan <44505932+RionelxSlovesky@users.noreply.github.com>
2023-05-15 10:21:15 +0600

    Created the about section
    Created the about section in the home page, will now work on the services section

---
+++ car-genius-client/src/Pages/Home/Services/Services.jsx
@@ -0,0 +1,13 @@
+
+
+const Services = () => {
+    return (
+        <div className="text-center">
+            <h3 className="text-2xl text-orange-600">Our Services</h3>
+            <h2 className="text-5xl">Our Service Areas</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt aspernatur neque quod laboriosam quaerat eos mollitia? <br /> Libero ducimus fuga dignissimos exercitationem ut. Quidem ducimus nemo mollitia repudiandae minima dicta aspernatur autem nobis praesentium nesciunt cum.</p>
+        </div>
+    );
+};
+
+export default Services;
