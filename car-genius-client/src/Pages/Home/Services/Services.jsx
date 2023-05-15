68d88371496744d3bf7ae4d59299c285817ae1fa
Ahmed Omar Salim Adnan <44505932+RionelxSlovesky@users.noreply.github.com>
2023-05-15 11:04:32 +0600

    Created the Service Section
    Created the service section; for now fetching the information from a fixed json file

--- car-genius-client/src/Pages/Home/Services/Services.jsx
+++ car-genius-client/src/Pages/Home/Services/Services.jsx
@@ -1,12 +1,31 @@
+import { useEffect, useState } from "react";
+import ServiceCard from "./ServiceCard/ServiceCard";
 
 
 const Services = () => {
+
+    const [services, setServices] = useState([])
+
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, [])
+
     return (
-        <div className="text-center">
-            <h3 className="text-2xl text-orange-600">Our Services</h3>
-            <h2 className="text-5xl">Our Service Areas</h2>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt aspernatur neque quod laboriosam quaerat eos mollitia? <br /> Libero ducimus fuga dignissimos exercitationem ut. Quidem ducimus nemo mollitia repudiandae minima dicta aspernatur autem nobis praesentium nesciunt cum.</p>
+        <div className="my-6">
+            <div className="text-center">
+                <h3 className="text-2xl text-orange-600">Our Services</h3>
+                <h2 className="text-5xl">Our Service Areas</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt aspernatur neque quod laboriosam quaerat eos mollitia? <br /> Libero ducimus fuga dignissimos exercitationem ut. Quidem ducimus nemo mollitia repudiandae minima dicta aspernatur autem nobis praesentium nesciunt cum.</p>
+            </div>
+            <div className="flex flex-wrap justify-center gap-6">
+                {
+                    services.map(service => <ServiceCard key={service._id} service={service}></ServiceCard>)
+                }
+            </div>
         </div>
+
     );
 };
 
