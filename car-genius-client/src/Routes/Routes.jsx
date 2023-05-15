520dae34096c3456f5e498dbd1a504b52528a7ad
Ahmed Omar Salim Adnan <44505932+RionelxSlovesky@users.noreply.github.com>
2023-05-16 05:42:00 +0600

    Implemented firebase authentication functionalities
    Now users can register and login

--- car-genius-client/src/Routes/Routes.jsx
+++ car-genius-client/src/Routes/Routes.jsx
@@ -5,6 +5,7 @@ import {
 import Main from '../Layout/Main'
 import Home from '../Pages/Home/Home'
 import Login from '../Pages/Login/Login'
+import Registration from '../Pages/Registration/Registration'
 
 const router = createBrowserRouter([
     {
@@ -18,6 +19,10 @@ const router = createBrowserRouter([
             {
                 path: '/login',
                 element: <Login></Login>
+            },
+            {
+                path: '/signup',
+                element: <Registration></Registration>
             }
         ]
     }
